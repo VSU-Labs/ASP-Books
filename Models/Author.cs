8f66bbe6411e7837e07ac1650211f635ee445d48
Diadlo <polsha3@gmail.com>
2016-09-26 18:25:53 +0300

    Added Author model

---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Books
+{
+	public class Author
+	{
+		public int AuthorId { get; set; }
+		public String Name { get; set; }		
+		public DateTime Birthday { get; set; }
+	}
+}
+
