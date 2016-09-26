c407a07a5e6a4ef05ab955e913ed02eebafc4e63
Diadlo <polsha3@gmail.com>
2016-09-26 16:36:30 +0300

    Added Book model

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Books.Models
+{
+	public class Book
+	{
+		public int BookId { get; set; }
+		public String Name { get; set; }
+		public DateTime PublishDate { get; set; }
+	}
+}
+
