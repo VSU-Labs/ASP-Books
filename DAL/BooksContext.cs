c0312caf9ced12072c92ca19598cabb2959faa08
Diadlo <polsha3@gmail.com>
2016-09-26 17:22:36 +0300

    Added BooksContext and BooksInitializer

---
+++ DAL/BooksContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Books.Models;
+
+namespace Books.DAL
+{
+	[DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+	public class BooksContext : DbContext
+	{
+		public BooksContext () : base("BooksContext")
+		{
+		}
+		static BooksContext ()
+		{
+			DbConfiguration.SetConfiguration(new MySql.Data.Entity.MySqlEFConfiguration());
+		}
+
+		public DbSet<Book> Books { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
+
---
+++ DAL/BooksInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+using Books.Models;
+
+namespace Books.DAL
+{
+	public class BooksInitializer : DropCreateDatabaseAlways<BooksContext>
+	{
+		protected override void Seed(BooksContext context) 
+		{
+			context.Books.Add (new Book { BookId = 1, Name = "Война и мир", PublishDate = new DateTime(1900, 1, 1) });
+			context.Books.Add (new Book { BookId = 2, Name = "Отцы и дети", PublishDate = new DateTime(1950, 1, 1) });
+			context.SaveChanges ();
+		}
+	}
+}
+
